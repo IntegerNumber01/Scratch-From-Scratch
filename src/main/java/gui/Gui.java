f4d932cf568cec4db9cb2665caceadbf8a791a74
Akshaykrishna10 <reach.akshayk10@gmail.com>
2026-05-01 11:51:43 -0700

    fixed file formats

---
+++ src/main/java/gui/Gui.java
@@ -0,0 +1,19 @@
+package src.main.java.gui;
+
+import backend.Sprite;
+import backend.World;
+
+import javafx.animation.AnimationTimer;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+
+public class Gui  
+{
+
+    
+}
