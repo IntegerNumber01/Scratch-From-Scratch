7d782734e406785648ab5f657b9b2f45e5aeb2d3
Akshaykrishna10 <reach.akshayk10@gmail.com>
2026-05-14 12:10:58 -0700

    did scratch value

--- src/main/java/backend/ScratchValue.java
+++ src/main/java/backend/ScratchValue.java
@@ -1,5 +1,35 @@
 package backend;
 
 public class ScratchValue {
+    //sets value to whatever is passed in constructor
+    private Object value ; 
+
+    public ScratchValue(double num)
+    {
+        value = num ; 
+    }
+
+    public ScratchValue(String word)
+    {
+        value = word ; 
+    }
+
+    public double toNumber()
+    {
+        if(isNumber())
+        {
+            return value ; 
+        } try {
+            return (double)
+        }
+    }
+
+    // helper method to differentiate between the type Double and String
+    public boolean isNumber()
+    {
+        return value instanceof Double ; 
+    }
+
+
     
 }
