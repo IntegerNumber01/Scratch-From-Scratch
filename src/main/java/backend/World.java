8daa9ab4396da0d54cc741dc3328806f28f082f5
IntegerNumber <124754713+IntegerNumber01@users.noreply.github.com>
2026-05-14 13:17:55 -0700

    Added global vars

--- src/main/java/backend/World.java
+++ src/main/java/backend/World.java
@@ -4,9 +4,12 @@ import java.util.*;
 public class World {
     private ArrayList<Sprite> sprites;
     private boolean isRunning;
+    private HashMap<String, ScratchValue> globalVariables;
+
     public World() {
         sprites = new ArrayList<Sprite>();
         isRunning = true ;
+        globalVariables = new HashMap<String, ScratchValue>();
     }
 
     public void addSprite(Sprite sprite)
@@ -27,5 +30,13 @@ public class World {
     public void stopProgram()
     {
         isRunning = false ;  
+    }
+
+    public void setGlobalVariable(String name, ScratchValue value) {
+        globalVariables.put(name, value);
+    }
+
+    public ScratchValue getGlobalVariable(String name) {
+        return globalVariables.get(name);
     }
 }
