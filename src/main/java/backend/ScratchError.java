68f47411cb79c7b323fde6ad1528d727e35b492c
IntegerNumber <124754713+IntegerNumber01@users.noreply.github.com>
2026-05-19 12:16:38 -0700

    Update ScratchError.java

---
+++ src/main/java/backend/ScratchError.java
@@ -0,0 +1,9 @@
+// this is a static class that has a throwError() method
+
+// the method takes (line text, line number, error message, OPTIONAL column number)
+
+
+
+// it should print the error line text, then do ^^^^^ in teh place that is wrong (using the colum number if there is one)
+
+// before returning, it should call System.exit(1).
